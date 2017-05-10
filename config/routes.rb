69a17dab16e8d30876dfb18bc1209e60dd7015dc
KisleK <kisle.kuhn1@gmail.com>
2017-05-10 14:19:28 -0500

    added model methods, fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+get '/contacts' => 'contacts#index'
+get '/' => 'contacts#index'
 
-get '/contact' => 'contacts#one_contact'
-get'/contacts' => 'contacts#{many_contacts}'
+ get '/contacts/new' => 'contacts#new'
+post '/contacts' => 'contacts#create'
+
+get 'contacts/:id' => 'contacts#show'
+
+get 'contacts/:id/edit' => 'contacts#edit'
+patch 'contact/:id' => 'contacts#update'
+
+delete 'contact/:id' => 'contacts#destroy'
 end
